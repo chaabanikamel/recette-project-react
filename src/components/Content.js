75a91d39a57d1050795448fc8e76b86eb15419be
chaabanikamel <chaabanik48@gmail.com>
2023-08-20 11:03:32 +0100

    mise en place du contenu

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import styles from "./Content.module.scss"
 
 function Content() {
   return (
-    <div className="flex-fill b3">Content</div>
+    <div className="flex-fill container p-20 ">
+      <h1 className="my-30 ">Découvrez nos nouvelles recettes </h1>
+      <div className= {`card p-20 ${styles.contentCard}`}>
+      <div className={styles.grid}>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>
+      <div className={styles.elem}>element</div>  
+      </div>
+      </div>
+  
+      
+    </div>
   )
 }
 
