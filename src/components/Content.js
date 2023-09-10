cfb28b5b0b7c9e187253415fe20282b31e7e9996
chaabanikamel <chaabanik48@gmail.com>
2023-09-10 21:27:23 +0100

    Rendre l'application responsive

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,28 +1,20 @@
 import styles from "./Content.module.scss"
 import Recipe from "./Recipe"
+import { data } from "../data/recipes"
 
 function Content() {
+  const recipes = data;
   return (
     <div className="flex-fill container p-20 ">
       <h1 className="my-30 ">Découvrez nos nouvelles recettes </h1>
-      <div className= {`card p-20 ${styles.contentCard}`}>
+      <div className= {`card p-20 ${styles.contentCard} `}>
       <div className={styles.grid}>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
+        { recipes.map((r)=>( 
+           <Recipe title={r.title} image={r.image}/>
+           )
+          
+        )}
+  
       
       </div>
       </div>
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,15 +1,22 @@
  import styles from "./Recipe.module.scss"
- import couscous from "../assets/images/couscous-viande.jpg"
-function Recipe() {
+ import { useState } from "react";
+ 
+function Recipe({title,image}) {
+ const [liked,setLiked]=useState(false);
+ function handleClick(){
+       setLiked(!liked)
+ }
   return (
-    <div className={styles.recipe}>
+    <div  onClick={handleClick} className={styles.recipe}>
       <div className={styles.imageContainer}>
-        <img src={couscous} alt="recipe" />
+        <img src={image} alt="recipe" />
       </div>
-      <div className={`${styles.recipeTitle} d-flex flex-row align-items-center justify-content-center`}>
-        <h3>
-        Couscous Viande
-        </h3>
+      <div className={`${styles.recipeTitle} d-flex flex-column align-items-center justify-content-center`}>
+        <h3 className="mb-10">
+          {title}
+          </h3>
+          <i className={`fa-solid fa-heart ${liked ? "text-primary":" "}`}></i>
+      
       </div>
     </div>
   )
